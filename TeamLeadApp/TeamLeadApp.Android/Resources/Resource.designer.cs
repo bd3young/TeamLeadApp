86860de7c4925237f1611e834b7da220616730b9
beaudeyoung <beaudeyoung@gmail.com>
2024-07-19 01:03:39 -0400

    new Icon

--- TeamLeadApp/TeamLeadApp.Android/Resources/Resource.designer.cs
+++ TeamLeadApp/TeamLeadApp.Android/Resources/Resource.designer.cs
@@ -18807,6 +18807,9 @@ namespace TeamLeadApp.Droid
 			// aapt resource value: 0x7F0C0002
 			public const int launcher_foreground = 2131492866;
 			
+			// aapt resource value: 0x7F0C0003
+			public const int TeamLeadAppIcon = 2131492867;
+			
 			static Mipmap()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
